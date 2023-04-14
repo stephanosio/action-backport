5fba5fc236c423f4b542688400bf420cbdc6637e
Stephanos Ioannidis <root@stephanos.io>
2023-04-14 22:55:20 +0900

    Output detailed log when backport fails
    This commit updates the backport action to provide more detailed error
    logs when backport fails by capturing the Git operation console outputs
    and passing them to the error handler.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- src/backport.ts
+++ src/backport.ts
@@ -97,7 +97,24 @@ const backportOnce = async ({
   title: string;
 }>): Promise<number> => {
   const git = async (...args: string[]) => {
-    await exec("git", args, { cwd: repo });
+    let output = '';
+    const options = {
+      listeners: {
+        stdout: (data: Buffer) => {
+          output += data.toString();
+        },
+        stderr: (data: Buffer) => {
+          output += data.toString();
+        }
+      },
+      cwd: repo
+    };
+
+    try {
+      await exec("git", args, options);
+    } catch {
+      throw new Error(output);
+    }
   };
 
   await git("switch", base);
@@ -315,7 +332,7 @@ const backport = async ({
             body: getFailedBackportCommentBody({
               base,
               commitSha: commitToBackport,
-              errorMessage: error.message,
+              errorMessage: error.message.replace(/^\s*[\r\n]/gm, ""),
               head,
             }),
             issue_number: number,
